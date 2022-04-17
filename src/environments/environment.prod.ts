dff6c089d97f8549ede67dafbab116ce8015959e
Lukman Oladosu <lukmanoladosu93@gmail.com>
2022-04-17 22:35:46 +0100

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  devUrl: {
+    // api where the news feeds are gotten from.
+    baseService: 'https://newsapi.org/v2/top-headlines?country=us',
+  },
 };
